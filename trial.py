5d718d2607bfe3b1fd3ee81926e0aa8a57ed7501
Bipin2005-dev <bipindehariya05@gmail.com>
2025-03-29 18:25:20 +0530

    File upload template for website now works!

--- trial.py
+++ trial.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import render_template
+from flask_cors import CORS
+from flask import request
 
 app = Flask("ProLogger")
+CORS(app)
 
-@app.route('/')
-def hello():
-    return '<p>Hello World!</p>'
+@app.route('/', methods=['POST'])
+def fileReceiver():
+    if request.method == "POST":
+        og_file = request.files['raw-log']
+        og_file.save(f"./uploadedFile.log")
+        return "<p>Yaaay!!</p>"
