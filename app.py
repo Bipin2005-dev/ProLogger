cf8be2913e0dd636417f7769f29a29441c651d1c
Bipin2005-dev <bipindehariya05@gmail.com>
2025-04-13 06:39:25 +0530

    Got the date range working in the table

--- app.py
+++ app.py
@@ -29,10 +29,39 @@ def analytics():
 def temp(filename: str):
     return send_from_directory('temp', filename)
 
-@app.route('/table', methods=['GET'])
+#Parsing Sun Dec 04 04:44:54 2005 to 2005-12-04T04:44:54
+def datetime_converter(og_date : str):
+    _, month, date, _time, year = og_date.split(" ")
+    month_dict = {
+    "Jan": "01",
+    "Feb": "02",
+    "Mar": "03",
+    "Apr": "04",
+    "May": "05",
+    "Jun": "06",
+    "Jul": "07",
+    "Aug": "08",
+    "Sep": "09",
+    "Oct": "10",
+    "Nov": "11",
+    "Dec": "12"
+    }
+    output = f"{year}-{month_dict[month]}-{date}T{_time}"
+    return output
+
+@app.route('/table', methods=['GET', 'POST'])
 def table():
-    tokenized_lines = np.loadtxt('temp/processedUpload.log', delimiter=',', skiprows=1, dtype=object)
-    return render_template('table.html', tokens = tokenized_lines)
+    if request.method == 'GET':
+        tokenized_lines = np.loadtxt('temp/processedUpload.log', delimiter=',', skiprows=1, dtype=object)
+        return render_template('table.html', tokens = tokenized_lines)
+
+    if request.method == 'POST':
+        tokenized_lines = np.loadtxt('temp/processedUpload.log', delimiter=',', skiprows=1, dtype=object)
+        start_datetime = f"{request.form['start-date']}T{request.form['start-time']}"
+        end_datetime = f"{request.form['end-date']}T{request.form['end-time']}"
+        mask = [True if (end_datetime >= datetime_converter(datetime) >= start_datetime) else False for datetime in tokenized_lines.T[1]]
+        tokenized_lines = tokenized_lines[mask]
+        return render_template('table.html', tokens = tokenized_lines)
 
 if __name__ == '__main__':
     app.run(debug=True)
